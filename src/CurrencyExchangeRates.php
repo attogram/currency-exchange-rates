b29e5a093ad754e83769ed097c3fdf607d270104
Attogram Project <attogram@users.noreply.github.com>
2019-04-13 22:19:31 +0200

    Attogram Currency Exchange Rates

--- src/CurrencyExchangeRates.php
+++ src/CurrencyExchangeRates.php
@@ -17,7 +17,7 @@ class CurrencyExchangeRates
     use CustomizationTrait;
 
     /** @var string Version*/
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2-pre';
 
     /** @var string Feeds Namespace */
     const FEEDS_NAMESPACE = "\\Attogram\\Currency\\Feeds\\";
