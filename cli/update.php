7d25cc56a8a933c8c13f280995085437f0f42a15
Attogram Project <attogram@users.noreply.github.com>
2019-04-14 09:33:10 +0200

    Currency Exchange Rates CLI Updater

--- cli/update.php
+++ cli/update.php
@@ -6,7 +6,8 @@ use Attogram\Currency\CurrencyExchangeRates;
 
 $autoload = '../vendor/autoload.php';
 if (!is_readable($autoload)) {
-    die('Vendor autoload file not found: ' . $autoload);
+    print 'Vendor autoload file not found: ' . $autoload . "\n";
+    exit(1);
 }
 
 /** @noinspection PhpIncludeInspection */
@@ -15,24 +16,27 @@ require_once $autoload;
 $code = $argv[1] ?? '';
 
 if (!Config::isValidFeed($code)) {
-    die(
-        "\nAttogram Currency Exchange Rates Updater v" . CurrencyExchangeRates::VERSION . "\n\n"
+    print "\nAttogram Currency Exchange Rates Updater v" . CurrencyExchangeRates::VERSION . "\n\n"
         . "Usage: php " . $argv[0] . " FeedCode\n\n"
         . "Available Feed Codes:\n  - "
-        . implode("\n  - ", array_keys(Config::$feeds)) . "\n\n"
-    );
+        . implode("\n  - ", array_keys(Config::$feeds)) . "\n";
+    exit(1);
 }
 
 $class = CurrencyExchangeRates::FEEDS_NAMESPACE . $code;
 if (!class_exists($class)) {
-    die('Class Not Found: ' . $class);
+    print 'Class Not Found: ' . $class . "\n";
+    exit(1);
 }
 
 $api = Config::getFeedApi($code);
 if (empty($api)) {
-    die($code . ' API Endpoint Not Found');
+    print $code . ' API Endpoint Not Found' . "\n";
+    exit(1);
 }
 
 $verbosity = 0;
 
 new $class($api, $verbosity);
+
+exit(0);
